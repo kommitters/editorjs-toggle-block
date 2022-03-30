5bd44c208a7db51817e17a66729c26e783baecc0
Julián David Sánchez Gallego <50778235+juliansg022@users.noreply.github.com>
2022-03-30 10:41:22 -0500

    Move method at the end of the class

--- src/index.js
+++ src/index.js
@@ -43,27 +43,6 @@ export default class ToggleBlock {
   }
 
   /**
-   * Adds listener in the editor to create a toggle
-   * through the '>' char and the 'Space' key
-   */
-  createToggleWithShortcut() {
-    const redactor = document.activeElement;
-    redactor.addEventListener('keyup', (e) => {
-      if (e.code === 'Space') {
-        const blockContainer = document.activeElement;
-        const content = blockContainer.textContent;
-        const { length } = content;
-
-        if ((content[0] === '>') && (length - 1 === 1)) {
-          const invocatorBlock = this.api.blocks.getCurrentBlockIndex();
-          this.api.blocks.insert('toggle', {}, this.api, invocatorBlock, true);
-          this.api.blocks.delete(invocatorBlock + 1);
-        }
-      }
-    });
-  }
-
-  /**
    * Render tool`s main Element and fill it with saved data
    *
    * @param {{data: object, api: object}}
@@ -529,5 +508,26 @@ export default class ToggleBlock {
     for (let i = toggleIndex; i < toggleIndex + blocks; i += 1) {
       this.api.blocks.delete(toggleIndex);
     }
+  }
+
+  /**
+   * Adds listener in the editor to create a toggle
+   * through the '>' char and the 'Space' key
+   */
+  createToggleWithShortcut() {
+    const redactor = document.activeElement;
+    redactor.addEventListener('keyup', (e) => {
+      if (e.code === 'Space') {
+        const blockContainer = document.activeElement;
+        const content = blockContainer.textContent;
+        const { length } = content;
+
+        if ((content[0] === '>') && (length - 1 === 1)) {
+          const invocatorBlock = this.api.blocks.getCurrentBlockIndex();
+          this.api.blocks.insert('toggle', {}, this.api, invocatorBlock, true);
+          this.api.blocks.delete(invocatorBlock + 1);
+        }
+      }
+    });
   }
 }
